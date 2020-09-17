78b825a0645d79834daa09a8b6d7fe0c39f328c6
David Lin <davidlin@live.nl>
2020-09-17 17:52:56 +0200

    new implementation done, users can add count, remove count and owner can set count

--- bot.py
+++ bot.py
@@ -1,30 +1,56 @@
 import os
 import random
-
+import sqlite3
 import discord
+import db
+
+from db import DB
+from sqlite3 import Error
 from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_SERVER')
+OWNER = os.getenv('DISCORD_OWNER')
 
 client = discord.Client()
 
-bot = commands.Bot(command_prefix='?')
+bot = commands.Bot(command_prefix='?', owner_id=int(OWNER))
 
-@client.event
+dbfilelocation = "./wordcount.db"
+
+def create_connection(dbfilename):
+    conn = None
+    try:
+        conn = sqlite3.connect(dbfilename)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    return conn
+
+dbconn = create_connection(dbfilelocation)
+
+@bot.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print("on_ready")
+    #print(
+    #    f'{client.user} is connected to the following guild:\n'
+    #    f'{guild.name}(id: {guild.id})'
+    #)
+    cur = dbconn.cursor()
+    createtable = \
+    "CREATE TABLE wordcount (id integer PRIMARY KEY, TEXT text DEFAULT NULL, COUNT integer DEFAULT 0)"
+    try:
+        cur.execute(createtable)
+    except Error as e:
+        print(e)
 
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
 
-def addCounter(filename):
+def addCountercheck(filename):
     filetest = open(filename,'a+')
     filetest.close()
     with open(filename, 'r+') as lines:
@@ -38,26 +64,92 @@ def addCounter(filename):
         countstring = str(count)
         lines.write(countstring)
 
-@client.event
+def filterNumbers(string):
+    tempstring = ""
+    for char in string:
+        if char.isdigit():
+            tempstring = tempstring + char
+    return tempstring
+
+@bot.event
 async def on_message(message):
     response = message.content.lower() + ' count: '
+    cur = None
+    print("on_message respond code")
     if message.author == client.user:
         return
-    if message.content.lower() == 'im the biggest nozomi fan' or message.content.lower() == 'i\'m the biggest nozomi fan':
-        addCounter('fan.txt')
-        response = response + open('fan.txt', 'r').read()
+    cur = dbconn.cursor()
+    tempstring = message.content.lower()
+    cur.execute("SELECT * FROM wordcount WHERE LOWER(TEXT)=?", (tempstring,))
+    rows = cur.fetchall()
+    print(rows)
+    if rows:
+        print("in rows")
+        cur.execute("UPDATE wordcount SET COUNT = COUNT + 1 WHERE LOWER(TEXT)=?", (tempstring,))
+        dbconn.commit()
+        cur.execute("SELECT COUNT FROM wordcount WHERE LOWER(TEXT)=?", (tempstring,))
+        rows = cur.fetchall()
+        temprows = str(rows[0])
+        temprows = filterNumbers(temprows)
+        response = response + temprows
         await message.channel.send(response)
-    if message.content.lower() == 'uwu':
-        addCounter('uwu.txt')
-        response = response + open('uwu.txt', 'r').read()
-        await message.channel.send(response)
-    if message.content.lower() == 'owo':
-        addCounter('owo.txt')
-        response = response + open('owo.txt', 'r').read()
-        await message.channel.send(response)
+    await bot.process_commands(message)
 
-#@bot.command(name='addCounter')
-#async def newCounter(letext):
-#    if 
+@bot.command()
+@commands.check_any(commands.has_role('Ultimate Weebs'), commands.is_owner())
+async def addcounter(ctx, letext):
+    print("in addcounter")
+    cur = dbconn.cursor()
+    cur.execute("SELECT 1 FROM wordcount WHERE LOWER(TEXT)=?", (letext.lower(),))
+    rows = cur.fetchall()
+    if not rows:
+        cur.execute("INSERT INTO wordcount (TEXT) VALUES (?)", (letext,))
+        dbconn.commit()
+        tempstring = letext + " succesfully added"
+    else:
+        tempstring = letext + " already exists"
+    print(tempstring)
+    await ctx.send(tempstring)
 
-client.run(TOKEN)
+@bot.command()
+@commands.check_any(commands.has_role('Ultimate Weebs'), commands.is_owner())
+async def delcounter(ctx, letext):
+    print("in delcounter")
+    cur = dbconn.cursor()
+    cur.execute("SELECT 1 FROM wordcount WHERE LOWER(TEXT)=?", (letext.lower(),))
+    rows = cur.fetchall()
+    if rows:
+        cur.execute("DELETE FROM wordcount WHERE TEXT=(?)", (letext,))
+        dbconn.commit()
+        tempstring = letext + " succesfully removed"
+    else:
+        tempstring = letext + " does not exists"
+    print(tempstring)
+    await ctx.send(tempstring)
+
+@bot.command()
+@commands.is_owner()
+async def setcounter(ctx, letext, amount):
+    if not letext or not amount:
+        await ctx.send("There are parameters missing. Usage: ?setcounter \"[text]\" [amount]")
+    else:
+        cur = dbconn.cursor()
+        cur.execute("SELECT * FROM wordcount WHERE LOWER(TEXT)=?", (letext,))
+        rows = cur.fetchall()
+        if rows:
+            amount = filterNumbers(amount)
+            if hasNumbers(amount):
+                cur.execute("UPDATE wordcount SET COUNT=? WHERE LOWER(TEXT)=?", (amount, letext,))
+                dbconn.commit()
+                await ctx.send("The count for " + letext + " has been set to " + amount)
+            else:
+                await ctx.send("Please enter a number as the second parameter")
+        else:
+            await ctx.send(letext + " cannot be found")
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CheckFailure):
+        await ctx.send("You do not have the rights for using this command")
+
+bot.run(TOKEN)
